e1f3e1230154a04a820c5bd9dbabbcf0d05c4290
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 15:57:15 +0300

    modified the task06

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход число и 
+// выводит эявляется ли число чётным(делися на 2 без остатка). Например:
+// 4 -> да
+// -3 -> нет
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = num % 2;
+if(res == 0) Console.Write($"Да, число '{num}' является чётным ");
+else Console.Write($"Нет, число '{num}' не является чётным ");
