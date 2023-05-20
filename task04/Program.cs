001b845cdfbd65295c634716bce0f70f9ed3ba37
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 14:11:40 +0300

    modified the task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход три числа и 
+// выдает максимальное из этих трёх чисел. Например:
+// 2, -3, 7 -> 7
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int[] array = { num1, num2, num3 };
+
+int max = num1;
+int count = 0;
+
+while (count < 3)
+{
+    if (array[count] > max)
+    {
+        max = array[count];
+        count++;
+    }
+}
+Console.WriteLine($"No, the square of {max} ");
