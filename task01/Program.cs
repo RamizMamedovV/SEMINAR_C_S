80270259a17bfe8dc469445e7a569d6543b69067
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 07:28:49 +0300

    modified the task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход 2 числа и проверяет
+// является 1ое число квадратом 2ого. Например:
+// а = 25, в = 5 -> да
+// а = 20, в = 5 -> нет
+
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 == num2 * num2) Console.WriteLine($"Yes, the square of {num2} = {num1}" );
+else Console.WriteLine($"No, the square of {num2} isn't equal to {num1}");
