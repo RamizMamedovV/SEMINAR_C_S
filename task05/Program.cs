0b994bdf3a1975f6cc28704ef0b52e83be81f3a7
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 10:50:05 +0300

    modified the task05

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход число (N) и 
+// выводит все целые числа от -N до N и на терминал. Например:
+// 4 -> "-4, -3,....3, 4"
+
+Console.Write("Enter some number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = -num;
+
+if (num > 0)
+    while (i <= num)
+    {
+        Console.Write($"{i++}, ");
+        //++i;
+    }
+else Console.Write("wrong number");
