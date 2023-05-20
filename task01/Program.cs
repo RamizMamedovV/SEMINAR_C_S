4c38a4bd4d891cdc796f8e0afa1dbc5e24d1c367
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 10:09:34 +0300

    added spaces in the task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -5,7 +5,10 @@
 
 Console.Write("Enter first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Enter second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+
 if(num1 == num2 * num2) Console.WriteLine($"Yes, the square of {num2} = {num1}" );
+
 else Console.WriteLine($"No, the square of {num2} isn't equal to {num1}");
