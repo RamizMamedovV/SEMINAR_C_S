97763057303c375b142e12bfcb1c929376f52a1c
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 10:07:45 +0300

    modified the task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход порядковый номер недели и 
+// выводит этот день недели на терминал. Например:
+// 1 -> пн
+
+Console.Write("Enter the number of week: ");
+string numOfWeek = Console.ReadLine();
+
+if (numOfWeek == "1") Console.Write("Mon");
+else if (numOfWeek == "2") Console.Write("Tue");
+else if (numOfWeek == "3") Console.Write("Wen");
+else if (numOfWeek == "4") Console.Write("Thu");
+else if (numOfWeek == "5") Console.Write("Fry");
+else if (numOfWeek == "6") Console.Write("Sat");
+else if (numOfWeek == "7") Console.Write("Sun");
+else Console.Write("wrong number");
