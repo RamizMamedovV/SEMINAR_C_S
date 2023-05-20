edfdb512b5863961b81862dbed78f6f8dee7304d
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 07:05:11 +0300

    added task into task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая на вход принимает число и выдает его квалрат
+// (число умноженное само на себя). Например:
+// 4 -> 16
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+if(num == 0) Console.WriteLine("You entered vrong num");
+else Console.WriteLine($"The square of your number = {square}" );
