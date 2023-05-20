8453bba639e90133722b9952083dd96b8eddca9d
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-20 13:15:02 +0300

    modified the task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход 2 числа и проверяет
+// какое число больше, а какое меньше. Например:
+// а = 5, в = 7 -> max =7
+// а = -20, в = -3 -> max = -3
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 ) 
+{
+    Console.WriteLine($"max = {num1}" );
+}
+else if(num1 == num2 ) 
+{
+    Console.WriteLine($"Номер {num1} = {num2}" );
+}
+else 
+{
+    Console.WriteLine($"max = {num2}" );
+}
