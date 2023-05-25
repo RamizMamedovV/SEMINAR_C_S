70428135d6b446a9b2d7b428826f7cb20a6bf941
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-25 18:02:14 +0300

    added task11

---
+++ task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите прогр. которая выводит случайное трехзначное число и 
+// пудаляет вторую цмфру этого числа. Например:
+// 768 -> 78
+
+int num = new Random().Next(100, 1000);
+Console.Write($"{num}-> ");
+Console.Write(DeleteSecondDigit(num));
+
+int DeleteSecondDigit(int number)
+{
+int dig1 = number / 100;
+int dig3 = number % 10;
+
+    return dig1 * 10 + dig3;
+}
+
