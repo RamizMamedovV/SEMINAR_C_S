8b1d6f94a2729dbe9f5e18e09f244f3c2cc92beb
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-25 08:06:06 +0300

    added a new file task09

---
+++ task09/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите прогр. которая выводит случайное число из отрезка [10, 99] и 
+// показывает наибольшую цмфру числа. Например:
+// 78 -> 8
+
+int num = new Random().Next(10, 100);
+Console.Write($"Из случайного числа: {num}, наибольшая цифра: ");
+
+int digital1 = num / 10;
+int digital2 = num % 10;
+
+if(digital1 > digital2) Console.Write(digital1);
+else Console.Write(digital2);
