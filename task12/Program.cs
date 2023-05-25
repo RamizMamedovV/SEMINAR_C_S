1c8a38eb58e28730e339128a62ae9e8af23794d8
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-25 20:20:34 +0300

    modified task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -12,12 +12,15 @@ Console.Write("Ведите второе целое число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 int remainder = Remainder(num1, num2);
-if(remainder > 0) Console.Write($"не кратно, остаток: {remainder}");
-else Console.Write("кратно ");
+if(remainder > 0) Console.Write($"Число {num1} не кратно, остаток: {remainder}");
+else if(remainder == 0) Console.Write($"Число {num1} кратно чису {num2} ");
+else Console.Write($"Число {num1} меньше числа {num2} ");
 
 int Remainder(int number1, int number2)
 {
-    int res = number1 % number2;
+    int res = 0;
+    if(number1 > number2) res = number1 % number2;
+    else res = -1;
 
     return res;
 }
