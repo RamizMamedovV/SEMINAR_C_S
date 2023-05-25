b2006abe9b0fb36a6a2113be2abe73e81e9d46bc
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-25 10:55:22 +0300

    modified the file task09

--- task09/Program.cs
+++ task09/Program.cs
@@ -5,8 +5,8 @@
 int num = new Random().Next(10, 100);
 Console.Write($"Из случайного числа в дпапазоне [10, 99]: {num}, наибольшая цифра-> ");
 
-int digital1 = num / 10;
-int digital2 = num % 10;
+int digit1 = num / 10;
+int digit2 = num % 10;
 
-if(digital1 > digital2) Console.Write(digital1);
-else Console.Write(digital2);
+if (digit1 > digit2) Console.Write(digit1);
+else Console.Write(digit2);
