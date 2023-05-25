28330b3113a7a6db63e37a5fcbffc5495f4456c6
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-25 19:59:32 +0300

    modified task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите прогр. которая принимает на вход 2 числа и выводит
+// является ли первое число кратным второму. Если нет, то
+// программа выводит остаток от деления.
+// Например:
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int remainder = Remainder(num1, num2);
+if(remainder > 0) Console.Write($"не кратно, остаток: {remainder}");
+else Console.Write("кратно ");
+
+int Remainder(int number1, int number2)
+{
+    int res = number1 % number2;
+
+    return res;
+}
