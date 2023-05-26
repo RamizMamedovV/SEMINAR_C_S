5edb8411bacb0c957e9177953174a669a3e9b94f
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-26 05:51:16 +0300

    added task14

---
+++ task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите прогр. которая принимает на вход число и 
+// проверяет, кратно ли оно одновременно 7 и 23. Например:
+// 14-> нет
+// 161 -> да
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool res = DevidedByTwoNumbers(num, 7, 23);
+Console.WriteLine(res ? "Да" : "Нет");
+
+bool DevidedByTwoNumbers(int devidedByTwoNum, int num1, int num2)
+{
+    return devidedByTwoNum % num1 == 0 && devidedByTwoNum % num2 == 0;
+}
