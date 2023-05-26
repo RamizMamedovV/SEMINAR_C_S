d65ab4a73f93cb426e63e93656d1e9b4b5e986de
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-26 09:55:21 +0300

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите прогр. которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет. Например:
+// 728 -> 8
+// 19 -> третьей цифры нет
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool check = IsThreeDigits(num);
+Console.WriteLine(check ? ThirdDigit(num) : "Третьей цифры нет");
+
+bool IsThreeDigits(int val)
+{
+    return val > 99 || val < -99 ? true : false;
+}
+
+int ThirdDigit(int number)
+{
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    number %= 10; 
+    return number;
+}
