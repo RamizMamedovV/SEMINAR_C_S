41c544a58c10c22c20fca7cdaa137734b3cc918f
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-26 07:30:50 +0300

    added task10

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите прогр. которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цмфру этого числа. Например:
+// 782 -> 8
+// 192 -> 9
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool res = IsThreeDigits(num);
+Console.WriteLine(res ? SecondDigit(num) : "Вы ввели не трёхзначное число: ");
+
+string SecondDigit(int number)
+{
+    int res = number / 10;
+    res = res % 10;
+    return $"Вторая цмфра этого числа: {res}";
+}
+
+
+bool IsThreeDigits( int val)
+{
+    return val > 99 && val < 1000 || val < -99 && val > -1000 ? true : false;
+}
