7a9085866a749d2be44b987cdb3b706ef83c1e01
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-29 13:57:13 +0300

    modified task20

--- task20/Program.cs
+++ task20/Program.cs
@@ -10,3 +10,26 @@
 
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты превой 'A' точки ");
+Console.Write("X: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты превой 'B' точки ");
+Console.Write("X: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int By = Convert.ToInt32(Console.ReadLine());
+
+double distance = Distance2D(Ax, Ay, Bx, By);
+double result = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine(result);
+
+double Distance2D(int ax, int ay, int bx, int by)
+{
+    double res = Math.Sqrt((bx - ax)* (bx - ax) + (by - ay)* (by - ay));
+
+    return res;
+}
