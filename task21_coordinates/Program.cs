882176e1be76dc768492a8dbab4b585fe4744844
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-29 23:08:00 +0300

    finished task21_coordinates

---
+++ task21_coordinates/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+string xofa = "Введите координаты X для точки А: ";
+string yofa = "Введите координаты Y для точки А: ";
+string zofa = "Введите координаты Z для точки А: ";
+
+string xofb = "Введите координаты X для точки B: ";
+string yofb = "Введите координаты Y для точки B: ";
+string zofb = "Введите координаты Z для точки B: ";
+
+string checkcontinue = "Нажмите '1' для продолжения или '2' для выхода: ";
+
+CheckContinue();
+
+void CheckContinue()
+{
+    Console.Write($"{checkcontinue}");
+    string str = Console.ReadLine();
+    if(str == "1") UserPrint();
+    else if(str == "2") return;
+    else CheckContinue();
+}
+
+void UserPrint()
+{
+    Console.Write(xofa);
+    int ax = Convert.ToInt32(Console.ReadLine());
+    Console.Write(yofa);
+    int ay = Convert.ToInt32(Console.ReadLine());
+    Console.Write(zofa);
+    int az = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write(xofb);
+    int bx = Convert.ToInt32(Console.ReadLine());
+    Console.Write(yofb);
+    int by = Convert.ToInt32(Console.ReadLine());
+    Console.Write(zofb);
+    int bz = Convert.ToInt32(Console.ReadLine());    
+
+    Distance3D(ax, ay, az, bx, by, bz);
+
+}
+
+void Distance3D(int ax, int ay, int az, int bx, int by, int bz)
+{
+    double distance = Math.Sqrt((bx -ax)*(bx -ax) + (by - ay)*(by - ay) +
+                                     (bz - az)*(bz - az));
+    double result = Math.Round(distance, 2, MidpointRounding.ToZero);
+    Console.WriteLine($" -> {result}");
+    CheckContinue();
+}
