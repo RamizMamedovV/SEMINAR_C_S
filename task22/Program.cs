f2d04f6cdf81100f6bb999bdc2cedd5f3bd10ce3
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-29 15:02:14 +0300

    modified task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -3,3 +3,27 @@
 // квадратов чисел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
+
+ PrintUser("Введите число 'положительное число больше нуля: ");
+
+void PrintUser(string mes)
+{
+    Console.WriteLine(mes);
+    int number = Convert.ToInt32(Console.ReadLine());
+    Tabl(number);
+}
+
+void Tabl(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i} -> {i * i}");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Введите только положительное число");
+    }
+}
