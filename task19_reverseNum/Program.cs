4285a2503c8080a6b53f91b6800b16147ae99b0c
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-30 20:27:27 +0300

    finished task19_reverseNum

--- task19_reverseNum/Program.cs
+++ task19_reverseNum/Program.cs
@@ -5,21 +5,43 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Write("Введите целое число: ");
-string val = Console.ReadLine();
-int len = val.Length;
-int number = Convert.ToInt32(val);
+// для ввода и вывода текста создаем текст на удобном языке
+string entermessage = "Введите целое число: ";
+string yes = "-> Верно, является палиндромом";
+string no = "Нет, не является палиндромом";
 
-Console.WriteLine(ReverseNum(number, len));
+User(entermessage);
 
-string ReverseNum(int num, int l)
+
+void User(string str)
+{
+    Console.Write(str);
+    string val = Console.ReadLine();
+    int len = CheckValue(val);
+    int number = Convert.ToInt32(val);
+    int res = ReverseNum(number, len);
+    if(res == number) Console.WriteLine($"{res} {yes}");
+    else Console.WriteLine($"{res} {no}");
+    //User(entermessage);                 
+     //мспользуется для рекурсии. в эом случае, для прерывания одновременно нажмите ctrl + c 
+}
+
+int ReverseNum(int num, int l)
 {
     int rnum = 0;
     for (int i = 0; i < l; i++)
     {
         rnum = rnum * 10 + num % 10;
         num = num / 10;
-        Console.WriteLine(rnum);
     }
-    return rnum == number ? "yes" : "no";
+    return rnum;
+}
+
+int CheckValue(string txt)
+{
+    for (int i = 0; i < txt.Length; i++)
+    {
+        if (txt[i] == '-') return txt.Length - 1;
+    }
+    return txt.Length;
 }
