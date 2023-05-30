c3d1f81af5a3c7e40129f843b1d5016cd4fed451
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-30 07:46:23 +0300

    adde a new file task19_reverseNum

---
+++ task19_reverseNum/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите целое число: ");
+string val = Console.ReadLine();
+int len = val.Length;
+int number = Convert.ToInt32(val);
+
+Console.WriteLine(ReverseNum(number, len));
+
+string ReverseNum(int num, int l)
+{
+    int rnum = 0;
+    for (int i = 0; i < l; i++)
+    {
+        rnum = rnum * 10 + num % 10;
+        num = num / 10;
+        Console.WriteLine(rnum);
+    }
+    return rnum == number ? "yes" : "no";
+}
