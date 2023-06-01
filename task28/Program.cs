5ae45df849967d46fdab3947af2567be53b85c53
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-01 16:02:22 +0300

    added a new task28

--- task28/Program.cs
+++ task28/Program.cs
@@ -3,3 +3,34 @@
 // произведение чисел от 1 до N.
 // 4 -> 24
 // 5 -> 120
+
+User();
+
+void User()
+{
+    Console.Write("Для выхода нажмите '0' или введите положительное число больше нуля: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number == 0) 
+    {
+        return;
+    }
+    else if(number < 0)
+    {
+         User();
+    }
+    else
+    {int result = Factorial(number);
+    Console.WriteLine($"произведение чисел от 1 до {number} -> {result}");
+    User();
+    }
+}
+
+int Factorial(int num)
+{
+    int res = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
