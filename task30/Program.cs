0dce55f75c1dbb6270d24f0f8febcf95bf1d1134
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-01 21:55:19 +0300

    added a new task30

--- task30/Program.cs
+++ task30/Program.cs
@@ -2,3 +2,24 @@
 // выводит массив из 8 элементов, заполненный
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+}
+int[] FillArray(int[] arr)
+{
+    Random num = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = num.Next(2);
+    }
+    return arr;
+}
