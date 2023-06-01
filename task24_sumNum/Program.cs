d65fadb06050561c7282f273a7177378c954629b
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-01 08:03:13 +0300

    finished task24_sumNum

--- task24_sumNum/Program.cs
+++ task24_sumNum/Program.cs
@@ -4,3 +4,40 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+
+string entermessage = "Введите целое, положительное число: ";
+string continuemessage = "Для выхода нажмите '0' или введите целое, положительное число: ";
+//string continuemessage = "Для выхлда нажмите '0' или введите целое, положительное число: ";
+
+User(entermessage);
+
+void User(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    CheckEnter(num);
+}
+
+// void User(int val, int num)
+// {
+//     Console.WriteLine($"{val,3} -> {num,4}");
+//     User(continuemessage);
+// }
+
+void CheckEnter(int num)
+{
+    if (num < 0) User(continuemessage);
+    else if (num == 0) return;
+    else SumNumber(num);
+}
+
+void SumNumber(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+        Console.WriteLine($"{i, 3} -> {sum,4}");
+    }
+    User(continuemessage);
+}
