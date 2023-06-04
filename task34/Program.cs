23ff2b6a72550588dd24cfb19136e3d5de4d775d
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-04 06:26:15 +0300

    modified task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -20,7 +20,7 @@ void User()
     System.Console.WriteLine($"Количество чётных чисел в массиве: {evenCount}");
     Console.WriteLine();
 
-    InputContinue("Введите '1' для продолжения или '0' - для выхода:");
+    InputContinue("Введите '1' для продолжения или '0' - для выхода: ");
 }
 
 int InputUserInt(string str)
@@ -31,41 +31,42 @@ int InputUserInt(string str)
     return num;
 }
 
-    void InputContinue(string str)
-    {
-        Console.Write(str);
-        int num = Convert.ToInt32(Console.ReadLine());
-        if (num == 1) User();
-        else if (num == 0) return;
-        else InputUserInt("Введите '1' для продолжения или '0' - для выхода:");
-    }
+void InputContinue(string str)
+{
+    Console.Write(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    if (num == 1) User();
+    else if (num == 0) return;
+    else InputContinue("Введите '1' для продолжения или '0' - для выхода: ");
+}
 
-    void ArrayPrint(int[] arr, string bracketSt = "[",
-                         string bracketEn = "]", string sep = ", ")
+void ArrayPrint(int[] arr, string bracketSt = "[",
+                     string bracketEn = "]", string sep = ", ")
+{
+    Console.Write(bracketSt);
+    for (int i = 0; i < arr.Length - 1; i++)
     {
-        Console.Write(bracketSt);
-        for (int i = 0; i < arr.Length - 1; i++)
-        {
-            Console.Write($"{arr[i]}{sep} ");
-        }
-        Console.WriteLine(arr[arr.Length - 1] + bracketEn);
+        Console.Write($"{arr[i]}{sep} ");
     }
+    Console.WriteLine(arr[arr.Length - 1] + bracketEn);
+}
 
-    int EvenCounter(int[] arr)
+int EvenCounter(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
     {
-        int counter = 0;
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] % 2 == 0) counter++;
-        }
-        return counter;
+        if (arr[i] % 2 == 0) counter++;
     }
+    return counter;
+}
 
-    void FillArray(int[] arr, int minElem = 100, int maxElem = 1000)
+void FillArray(int[] arr, int minElem = 100, int maxElem = 1000)
+{
+    var rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
     {
-        var rnd = new Random();
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] = rnd.Next(minElem, maxElem);
-        }
+        arr[i] = rnd.Next(minElem, maxElem);
     }
+}
