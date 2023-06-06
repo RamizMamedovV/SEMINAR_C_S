e80be5b36929a707c3deb00dd6a80b4ce7ec5462
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-06 20:40:36 +0300

    modified task40

--- task40/Program.cs
+++ task40/Program.cs
@@ -3,3 +3,10 @@
 // такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон.
+
+Console.WriteLine(IsTriangle(55, 4, 4) ? "yes" : "no");
+
+bool IsTriangle(int sideA, int sideB, int sideC)
+{
+    return sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideB + sideA;
+}
