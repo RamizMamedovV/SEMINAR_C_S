83f4e064494fa34be7097f67c962500af712d2ab
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-07 17:20:21 +0300

    modified task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -19,3 +19,34 @@ while (true)
         return;
     }
 }
+
+// int[] ParseString(string input)
+// {
+//     string[] strArray = input.Split(", ");
+//     int[] intArray = new int[strArray.Length];
+//     for (int i = 0; i < strArray.Length; i++)
+//     {
+//         intArray[i] = Convert.ToInt32(strArray[i]);
+//     }
+//     return intArray;
+// }
+
+// Console.WriteLine("Enter several numbers as follows: <num1, num2, ..., numN>");
+// string user_str = Console.ReadLine()!;
+
+// if (user_str != string.Empty)
+// {
+//     int[] array = ParseString(user_str);
+
+//     int count = 0;
+//     foreach (var num in array)
+//     {
+//         if (num > 0) count++;
+//     }
+
+//     Console.WriteLine($"You have entered {count} positive numbers");
+// }
+// else
+// {
+//     Console.WriteLine("You have entered 0 positive numbers");
+// }
