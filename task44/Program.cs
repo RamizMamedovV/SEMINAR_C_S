ebfffd0a9bccdcc880b270625a9eb18350037af8
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-07 13:41:12 +0300

    modified task44

--- task44/Program.cs
+++ task44/Program.cs
@@ -7,25 +7,63 @@
 Console.Write("Введите число больше нуля: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 0) Fibonacci(number);
-else Console.WriteLine("Некорректный ввод!");
-
-void Fibonacci(int num)
+if (number > 0)
 {
-    int first = 0;
-    int second = 1;
-    if (num == 1) System.Console.WriteLine($"{first}");
-    else if (num == 2) System.Console.WriteLine($"{first}{second}");
+    if (number == 1)
+    {
+        int[] array1 = new int[1];
+        ArrayPrintInt(array1);
+    }
     else
     {
-        int res = 0;
-        Console.Write($"{first}{second}");
-        for (int i = 3; i < num + 1; i++)
-        {
-            res = first + second;
-            Console.Write(res);
-            first = second;
-            second = res;
-        }
+        int[] array = FibonacciArrayCreaterInt(number);
+        ArrayPrintInt(array);
     }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
+//if (number > 0) Fibonacci(number);
+// else Console.WriteLine("Некорректный ввод!");
+// void Fibonacci(int num)
+// {
+//     int first = 0;
+//     int second = 1;
+//     if (num == 1) System.Console.WriteLine($"{first}");
+//     else if (num == 2) System.Console.WriteLine($"{first}{second}");
+//     else
+//     {
+//         int res = 0;
+//         Console.Write($"{first}{second}");
+//         for (int i = 3; i < num + 1; i++)
+//         {
+//             res = first + second;
+//             Console.Write(res);
+//             first = second;
+//             second = res;
+//         }
+//     }
+// }
+
+int[] FibonacciArrayCreaterInt(int sizeNum = 8)
+{
+    int[] arr = new int[sizeNum];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void ArrayPrintInt(int[] arr, string startBracket = "[", string lastBracket = "]")
+{
+    Console.Write(startBracket);
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}{lastBracket}");
 }
