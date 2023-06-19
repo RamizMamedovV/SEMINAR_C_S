28c20b56cec6666ef186d5b43f57f022852797b3
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-19 22:24:18 +0300

    modified task67

---
+++ task67/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 67.Напишите программу, которая будет принимать
+//  на вход 2 числа и возвращать сумму его цифр.
+//
+// 453 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigitsRecursion(number);
+System.Console.WriteLine($"Сумма цифр в числе {number} = {result}");
+
+int SumDigitsRecursion(int num)
+{
+    int sum = 0;
+    if (num == 0) return;
+    sum += num % 10;
+    SumDigitsRecursion(num / 10);
+
+    return sum;
+}
+
+// int SumDigitsRecursion(int num)
+// {
+//     int sum = 0;
+//     int n = num;
+//     while (n > 0)
+//     {
+//         sum += n % 10;
+//         n = n / 10;
+//     }
+
+//     return sum;
+// }
