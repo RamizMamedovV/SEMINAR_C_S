0df66c860365dded78a92af041de84bcfdeb9581
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-19 11:28:37 +0300

    completed task60

--- task60/Program.cs
+++ task60/Program.cs
@@ -6,9 +6,11 @@
 // 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
 // 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
 
-int[,,] Matrix3DIntCreater(int depth, int rows, int columns, int minVal = 10, int maxVal = 99)
+int[,,] Matrix3DIntCreater(int depth, int rows, int columns)
 {
     int[,,] mtx = new int[depth, rows, columns];
+    int minVal = 10;
+    int maxVal = 100;
 
     for (int i = 0; i < mtx.GetLength(0); i++)
     {
@@ -16,6 +18,7 @@ int[,,] Matrix3DIntCreater(int depth, int rows, int columns, int minVal = 10, in
         {
             for (int k = 0; k < mtx.GetLength(2); k++)
             {
+                if(minVal == maxVal) break;
                 mtx[i, j, k] = minVal++;
             }
         }
@@ -40,6 +43,6 @@ void Matrix3DIntPrint(int[,,] mtx)
     }
 }
 
-int[,,] mart3D = Matrix3DIntCreater(3, 4, 5);
+int[,,] matrix3D = Matrix3DIntCreater(4, 4, 5);
 
-Matrix3DIntPrint(mart3D);
+Matrix3DIntPrint(matrix3D);
